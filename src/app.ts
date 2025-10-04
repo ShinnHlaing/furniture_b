d5fbc7f26adb8e3a2af66adef557d3f681db42d5
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-10-04 23:38:32 +0630

    update

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import helmet from "helmet";
 import cors from "cors";
 import compression from "compression";
 import morgan from "morgan";
+
 export const app = express();
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
+app
+  .use(morgan("dev"))
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cors()) //cross origin resouce sharing
+  .use(helmet()) //for browser security
+  .use(compression()); //
