689ab578893aa33afbecf312e5f33304f660afa3
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-10-13 22:25:01 +0630

    update: some

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 import compression from "compression";
 import morgan from "morgan";
-
+import { limiter } from "./middlewares/rateLimiter";
 export const app = express();
 app
   .use(morgan("dev"))
@@ -11,4 +11,9 @@ app
   .use(express.json())
   .use(cors()) //cross origin resouce sharing
   .use(helmet()) //for browser security
-  .use(compression()); //
+  .use(compression()) //zip the respond bodies for more faster respond
+  .use(limiter);
+//http://localhost:8080/health
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "hello we are ready for response." });
+});
---
+++ src/middlewares/rateLimiter.ts
@@ -0,0 +1,10 @@
+import rateLimit from "express-rate-limit";
+
+export const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 15, // Limit each IP to 15 requests per `window` (here, per 1 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
